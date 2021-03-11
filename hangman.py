81e0c271df5e5f46704d5279dc81ec33aa1162f0
Joshua Krueger <jkru969@gmail.com>
2021-03-11 00:34:00 -0500

    Added a condition to win at he start of loop, checks if progress is equal to answer, if so the game ends and the user wins. removed an unnecessary line (33) as it added the guessed letter twice. Realized the indentation was one too far on line 38, so it was pushed back one along with the two following statements.

--- hangman.py
+++ hangman.py
@@ -14,6 +14,9 @@ def game(user_word):  # main game function. Running this will start the game.
     guessed_letters = []  # Keeps track of letters guessed
     fails = 6  # Counter to keep track of attempts remaining
     while fails > 0:  # central game loop, essentially saying "as long as there is at least one limb left, go again."
+        if progress == answer:
+            print(f"you did it!\nThe word was {word}")
+            exit()
         print(f"\n{hangman_image[fails]}"
               f"\nYou have {fails} tries left"
               f"\nYour word so far is {progress}"
@@ -24,18 +27,17 @@ def game(user_word):  # main game function. Running this will start the game.
                 print(f"You already guessed {current_guess}!")  # stops the user from wasting a limb on a letter they've already tried
             elif current_guess in answer:  # only happens if the guessed letter is in the answer
                 for index, character in enumerate(answer):  # enumerates answer making two dicts full of values for the answer list.
-                    progress = list(progress)  # MAYBE NOT NEEDED
+                    progress = list(progress)  # Ensures that only one letter is added where it should be each time.
                     if character == current_guess:  # checks the guess against each letter in answer
                         progress[index] = current_guess  # sets that spot in the list of underscores to the guess if correct
-                        guessed_letters.append(current_guess)  # adds the letter that was guessed to guessed letters
             elif current_guess not in answer:  # checks if the guess is not in the answer
                 fails -= 1  # they lose one attempt and a limb is added
             guessed_letters.append(current_guess)  # adds the letter that was guessed to guessed letters
         else:
             print("Only single letter guesses allowed.")  # in case they try to guess more than a letter
-        print(f"{hangman_image[0]}"  # only occurs when they lose and the loop ends, prints the final position of the gallows
-              f"\nSorry, you failed to save the person"
-              f"\nYour word was {word}, better luck next time!")  # Closing messages
+    print(f"{hangman_image[0]}"  # only occurs when they lose and the loop ends, prints the final position of the gallows
+          f"\nSorry, you failed to save the person"
+          f"\nYour word was {word}, better luck next time!")  # Closing messages
 
 
 game(input("What word shall we test? "))  # Calls the main game function asking for a word
