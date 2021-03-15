ee916207fcbdcb2f9e907388ca5de8136c0dede5
Joshua Krueger <jkru969@gmail.com>
2021-03-15 09:36:03 -0400

    Added a comment to tell the user important info about the nltk package.

--- word_list.py
+++ word_list.py
@@ -1,13 +1,13 @@
 import nltk  # imports libraries needed to have all of the words that have ever been in the English language
 import ssl
 
-try:
+"""try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
     pass
 else:
     ssl._create_default_https_context = _create_unverified_https_context    # workaround to grab the needed package
-nltk.download('words')
+nltk.download('words')"""  # You only have to run this whole thing every once in a while as the list updates, but once it has run once the list will be on your computer
 
 from nltk.corpus import words   # imports all the words and dumps them into word_list
 game_words = words.words()
